71a855b7b49441580f6c9671d88a490282811e4a
procoder <procoder@promail>
2020-02-11 12:52:52 +0300

    initial git repository

---
+++ script.js
@@ -0,0 +1,15 @@
+var userName = prompt('Введите ваше имя');
+var userAge  = prompt('Введите ваш возраст');
+var task_1   = prompt('7 + 3 = ?');
+var task_2   = prompt('7 - 4 = ?');
+var task_3   = prompt('7 * 3 = ?');
+var task_4   = prompt('7 / 7 = ?');
+ 
+alert('Откройте консоль для просмотра');
+
+console.log(userName);
+console.log(userAge);
+console.log('7 + 3 = 10 Ваш ответ: ' +task_1);
+console.log('7 - 4 = 3  Ваш ответ: ' +task_2);
+console.log('7 * 3 = 21 Ваш ответ: ' +task_3);
+console.log('7 / 7 = 1  Ваш ответ: ' +task_4);
